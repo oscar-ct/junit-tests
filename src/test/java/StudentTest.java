cb7b2ef359cc77ca240a2934a951449a59b88ce5
Oscar Castro <oscar.a.castro818@gmail.com>
2022-04-14 22:11:50 -0500

    feat: added junit testing for all methods, constructors, getters, and setters for Student class

--- src/test/java/StudentTest.java
+++ src/test/java/StudentTest.java
@@ -1,4 +1,65 @@
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertSame;
+
 public class StudentTest {
+
+
+    @Test
+    public void testGetId() {
+        ArrayList<Integer> grades = new ArrayList<>();
+        grades.add(90);
+        grades.add(95);
+        grades.add(100);
+        Student firstStudent = new Student(1, "oscar", grades);
+        assertEquals(firstStudent.id, firstStudent.getId());
+}
+
+    @Test
+    public void testGetName() {
+        ArrayList<Integer> grades = new ArrayList<>();
+        grades.add(90);
+        grades.add(95);
+        grades.add(100);
+        Student firstStudent = new Student(1, "oscar", grades);
+        assertEquals(firstStudent.name, firstStudent.getName());
+    }
+
+    @Test
+    public void testAddGrade() {
+        ArrayList<Integer> grades = new ArrayList<>();
+        grades.add(90);
+        grades.add(95);
+        grades.add(100);
+        Student firstStudent = new Student(1, "oscar", grades);
+
+    }
+
+    @Test
+    public void testGetGrade() {
+        ArrayList<Integer> grades = new ArrayList<>();
+        grades.add(90);
+        grades.add(95);
+        grades.add(100);
+        Student firstStudent = new Student(1, "oscar", grades);
+        assertEquals(firstStudent.grades, firstStudent.getGrades());
+    }
+
+
+    @Test
+    public void testGradeAverage() {
+        ArrayList<Integer> grades = new ArrayList<>();
+        grades.add(90);
+        grades.add(95);
+        grades.add(100);
+        Student firstStudent = new Student(1, "oscar", grades);
+        assertEquals(95, firstStudent.getGradeAverage(), 0.01);
+    }
+
 
 
 }
